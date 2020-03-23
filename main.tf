94bfa258021c310b9c91b18a9b75688226b672c4
jirishus <jirishus@yahoo.com>
2020-03-22 20:55:25 -0700

    provider alias for multiple regions

--- main.tf
+++ main.tf
@@ -2,47 +2,54 @@
 ## Variables
 ###################
 
-variable "region" {
-  default = "us-east-1"
-}
+# variable "region" {
+#   default = "us-east-1"
+# }
 
 ###################
 ## Data
 ###################
 
-data "aws_ami" "aws-linux" {
-  most_recent = true
-  owners      = ["amazon"]
+# data "aws_ami" "aws-linux" {
+#   most_recent = true
+#   owners      = ["amazon"]
 
-  filter {
-    name   = "name"
-    values = ["amzn-ami-hvm*"]
-  }
+#   filter {
+#     name   = "name"
+#     values = ["amzn-ami-hvm*"]
+#   }
 
-  filter {
-    name   = "root-device-type"
-    values = ["ebs"]
-  }
+#   filter {
+#     name   = "root-device-type"
+#     values = ["ebs"]
+#   }
 
-  filter {
-    name   = "virtualization-type"
-    values = ["hvm"]
-  }
-}
+#   filter {
+#     name   = "virtualization-type"
+#     values = ["hvm"]
+#   }
+# }
 
 ###################
 ## Providers
 ###################
 
+# The default provider configuration
 provider "aws" {
-  region = var.region
+  region = "us-east-1"
+}
+
+# Additional provider configuration for west coast region
+provider "aws" {
+  alias  = "us_west_2"
+  region = "us-west-2"
+}
+
+provider "aws" {
+  alias  = "us_east_2"
+  region = "us-east-2"
 }
 
 ###################
 ## Resources
 ###################
-
-resource "aws_instance" "primary_app_server" {
-  ami           = data.aws_ami.aws-linux.id
-  instance_type = "t2.micro"
-}
---
+++ use2_ec2.tf
@@ -0,0 +1,29 @@
+###################
+## Resources
+###################
+resource "aws_instance" "primary_app_server_use2" {
+  provider      = aws.us_east_2
+  ami           = data.aws_ami.centos7.id
+  instance_type = "t2.micro"
+}
+
+data "aws_ami" "centos7" {
+  provider    = aws.us_east_2
+  owners      = ["679593333241"]
+  most_recent = true
+
+  filter {
+    name   = "name"
+    values = ["CentOS Linux 7 x86_64 HVM EBS ENA 1803_01-b7ee8a69*"]
+  }
+
+  filter {
+    name   = "architecture"
+    values = ["x86_64"]
+  }
+
+  filter {
+    name   = "root-device-type"
+    values = ["ebs"]
+  }
+}
