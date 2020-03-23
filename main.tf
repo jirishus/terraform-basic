e49fe75b19a9e62facd2dcadf98546b8662f3a6e
jirishus <jirishus@yahoo.com>
2020-03-22 19:04:54 -0700

    basic linux ami and ec2 instance

--- main.tf
+++ main.tf
@@ -6,6 +6,29 @@ variable "region" {
   default = "us-east-1"
 }
 
+###################
+## Data
+###################
+
+data "aws_ami" "aws-linux" {
+  most_recent = true
+  owners      = ["amazon"]
+
+  filter {
+    name   = "name"
+    values = ["amzn-ami-hvm*"]
+  }
+
+  filter {
+    name   = "root-device-type"
+    values = ["ebs"]
+  }
+
+  filter {
+    name   = "virtualization-type"
+    values = ["hvm"]
+  }
+}
 
 ###################
 ## Providers
@@ -13,4 +36,13 @@ variable "region" {
 
 provider "aws" {
   region = var.region
+}
+
+###################
+## Resources
+###################
+
+resource "aws_instance" "primary_app_server" {
+  ami           = data.aws_ami.aws-linux.id
+  instance_type = "t2.micro"
 }
