1ec1cd3686fe9fc118f43ef8181d25966a3f4d56
jirishus <jirishus@yahoo.com>
2020-03-22 21:34:57 -0700

    low level network settings for usw2

--- network/main.tf
+++ network/main.tf
@@ -16,16 +16,25 @@ provider "aws" {
 }
 
 # Additional provider configuration for west coast region
-# provider "aws" {
-#   alias  = "us_west_2"
-#   region = "us-west-2"
-# }
-
-# provider "aws" {
-#   alias  = "us_east_2"
-#   region = "us-east-2"
-# }
+provider "aws" {
+  alias  = "us_west_2"
+  region = "us-west-2"
+}
 
 ###################
 ## Resources
 ###################
+
+resource "aws_vpc" "devops_usw2" {
+  provider = aws.us_west_2
+  # Specify primary CIDR block for this VPC
+  cidr_block = "10.0.0.0/16"
+}
+
+###################
+## Outputs
+###################
+
+output "primary_vpc_usw2" {
+  value = aws_vpc.devops_usw2.id
+}
